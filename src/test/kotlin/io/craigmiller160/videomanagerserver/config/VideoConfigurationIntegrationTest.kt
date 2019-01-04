2731e064a85a63637ff1cc8e2d68bdc49a871f90
Craig Miller <craigmiller160@gmail.com>
2019-01-03 19:59:56 -0500

    Built some configuration integration tests

---
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/config/VideoConfigurationIntegrationTest.kt
@@ -0,0 +1,32 @@
+package io.craigmiller160.videomanagerserver.config
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class VideoConfigurationIntegrationTest {
+
+    companion object {
+        private const val VLC_COMMAND = "VLC_COMMAND"
+        private const val FILE_PATH_ROOT = "FILE_PATH_ROOT"
+    }
+
+    @Autowired
+    private lateinit var videoConfig: VideoConfiguration
+
+    @Test
+    fun testVlcCommand() {
+        assertEquals(VLC_COMMAND, videoConfig.vlcCommand)
+    }
+
+    @Test
+    fun testFilePathRoot() {
+        assertEquals(FILE_PATH_ROOT, videoConfig.filePathRoot)
+    }
+
+}
