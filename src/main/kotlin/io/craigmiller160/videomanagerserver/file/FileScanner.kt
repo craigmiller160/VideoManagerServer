a1ecde0284355178f9c3c57a639ac44acd9469d4
Craig Miller <craigmiller160@gmail.com>
2019-01-05 15:34:12 -0500

    Added coroutine dependency and created first draft of FileScanner.

---
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/file/FileScanner.kt
@@ -0,0 +1,41 @@
+package io.craigmiller160.videomanagerserver.file
+
+import io.craigmiller160.videomanagerserver.config.VideoConfiguration
+import io.craigmiller160.videomanagerserver.repository.VideoFileRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.nio.file.Files
+import java.nio.file.Paths
+
+@Component
+class FileScanner @Autowired constructor(
+        private val videoConfig: VideoConfiguration,
+        private val videoFileRepo: VideoFileRepository
+) {
+
+    fun scanForFiles(done: () -> Unit) {
+        val filePathRoot = videoConfig.filePathRoot
+        val fileExts = videoConfig.splitFileExts()
+
+        runBlocking {
+            launch(Dispatchers.IO) {
+                Files.walk(Paths.get(filePathRoot))
+                        .filter { p -> !p.toFile().isDirectory }
+                        .filter { p -> !p.toFile().isHidden }
+                        .filter { p -> fileExts.contains(p.toFile().extension) }
+                        .map { p -> p.toString().replace(Regex("^$filePathRoot"), "") }
+                        .forEach { name -> videoFileRepo.mergeVideoFilesByName(name) }
+            }
+        }
+    }
+
+}
+
+fun main(args: Array<String>) {
+    Files.walk(Paths.get(System.getProperty("user.home")))
+            .limit(5)
+            .forEach { println(it) }
+}
