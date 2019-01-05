fe67f70109e0d1a6a3a7eb349099248cf73b87ef
Craig Miller <craigmiller160@gmail.com>
2019-01-05 18:02:59 -0500

    Added uncaught exception handler

--- src/main/kotlin/io/craigmiller160/videomanagerserver/VideoManagerServerApplication.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/VideoManagerServerApplication.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.videomanagerserver
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import java.io.File
@@ -7,7 +8,10 @@ import java.io.File
 @SpringBootApplication
 class VideoManagerServerApplication
 
+private val logger = LoggerFactory.getLogger(VideoManagerServerApplication::class.java)
+
 fun main(args: Array<String>) {
+    Thread.setDefaultUncaughtExceptionHandler { _, e -> logger.error("Uncaught exception", e) }
     runApplication<VideoManagerServerApplication>(*args)
 }
 
