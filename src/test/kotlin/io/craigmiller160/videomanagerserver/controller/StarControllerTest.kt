10fe3f16d63def5f28fe0b13ad0423b43c35fd26
Craig Miller <craigmiller160@gmail.com>
2019-01-05 13:48:02 -0500

    Fully implemented the StarController tests

--- src/test/kotlin/io/craigmiller160/videomanagerserver/controller/StarControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/controller/StarControllerTest.kt
@@ -1,12 +1,17 @@
 package io.craigmiller160.videomanagerserver.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.craigmiller160.videomanagerserver.dto.Star
 import io.craigmiller160.videomanagerserver.service.StarService
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 import org.springframework.boot.test.json.JacksonTester
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import java.util.Optional
 
 class StarControllerTest {
 
@@ -16,7 +21,7 @@ class StarControllerTest {
     @Mock
     private lateinit var starService: StarService
 
-    private lateinit var seriesController: StarController
+    private lateinit var starController: StarController
 
     private lateinit var jacksonStarList: JacksonTester<List<Star>>
     private lateinit var jacksonStar: JacksonTester<Star>
@@ -29,32 +34,83 @@ class StarControllerTest {
 
     @Before
     fun setup() {
-        TODO("Finish this")
+        starNoId = Star(starName = "NoId")
+        star1 = Star(1, "FirstStar")
+        star2 = Star(2, "SecondStar")
+        star3 = Star(3, "ThirdStar")
+        starList = listOf(star1, star2, star3)
+
+        MockitoAnnotations.initMocks(this)
+        JacksonTester.initFields(this, ObjectMapper())
+
+        starController = StarController(starService)
+        mockMvc = MockMvcBuilders.standaloneSetup(starController).build()
+        mockMvcHandler = MockMvcHandler(mockMvc)
     }
 
     @Test
     fun testGetAllStars() {
-        TODO("Finish this")
+        `when`(starService.getAllStars())
+                .thenReturn(starList)
+                .thenReturn(listOf())
+
+        var response = mockMvcHandler.doGet("/stars")
+        assertOkResponse(response, jacksonStarList.write(starList).json)
+
+        response = mockMvcHandler.doGet("/stars")
+        assertNoContentResponse(response)
     }
 
     @Test
     fun testGetStar() {
-        TODO("Finish this")
+        `when`(starService.getStar(1))
+                .thenReturn(Optional.of(star1))
+        `when`(starService.getStar(5))
+                .thenReturn(Optional.empty())
+
+        var response = mockMvcHandler.doGet("/stars/1")
+        assertOkResponse(response, jacksonStar.write(star1).json)
+
+        response = mockMvcHandler.doGet("/stars/5")
+        assertNoContentResponse(response)
     }
 
     @Test
     fun testAddStar() {
-        TODO("Finish this")
+        val starWithId = starNoId.copy(starId = 1)
+        `when`(starService.addStar(starNoId))
+                .thenReturn(starWithId)
+
+        val response = mockMvcHandler.doPost("/stars", jacksonStar.write(starNoId).json)
+        assertOkResponse(response, jacksonStar.write(starWithId).json)
     }
 
     @Test
     fun testUpdateStar() {
-        TODO("Finish this")
+        val updatedCategory = star2.copy(starId = 1)
+        `when`(starService.updateStar(1, star2))
+                .thenReturn(Optional.of(updatedCategory))
+        `when`(starService.updateStar(5, star3))
+                .thenReturn(Optional.empty())
+
+        var response = mockMvcHandler.doPut("/stars/1", jacksonStar.write(star2).json)
+        assertOkResponse(response, jacksonStar.write(updatedCategory).json)
+
+        response = mockMvcHandler.doPut("/stars/5", jacksonStar.write(star3).json)
+        assertNoContentResponse(response)
     }
 
     @Test
     fun testDeleteStar() {
-        TODO("Finish this")
+        `when`(starService.deleteStar(1))
+                .thenReturn(Optional.of(star1))
+                .thenReturn(Optional.empty())
+
+        var response = mockMvcHandler.doDelete("/stars/1")
+        assertOkResponse(response, jacksonStar.write(star1).json)
+
+        response = mockMvcHandler.doDelete("/stars/5")
+        assertNoContentResponse(response)
     }
 
 }
