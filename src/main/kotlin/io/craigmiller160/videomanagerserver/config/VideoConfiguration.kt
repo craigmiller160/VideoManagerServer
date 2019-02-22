2a1b00e934d709970e51ed65a8d076c15585e31c
Craig Miller <craigmiller160@gmail.com>
2019-02-21 21:15:21 -0500

    Modified the development properties to require an environment variable to set the path to the video file directory. This way development can be done more easily on multiple systems.

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/VideoConfiguration.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/VideoConfiguration.kt
@@ -2,12 +2,15 @@ package io.craigmiller160.videomanagerserver.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
+import org.springframework.validation.annotation.Validated
+import javax.validation.constraints.NotBlank
 
 @Configuration
+@Validated
 @ConfigurationProperties(prefix = "video")
 data class VideoConfiguration (
         var vlcCommand: String = "",
+        @field:NotBlank
         var filePathRoot: String = "",
         var apiPageSize: Int = 0,
         var fileExts: String = ""
