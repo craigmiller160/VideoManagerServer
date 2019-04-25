17289b1b88be36ed9467ffa3c384d3c39351d3ce
Craig Miller <craigmiller160@gmail.com>
2019-04-25 16:45:52 -0400

    Added fileAdded to SortBy

--- src/main/kotlin/io/craigmiller160/videomanagerserver/dto/SortBy.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/dto/SortBy.kt
@@ -4,5 +4,6 @@ enum class SortBy(val orderByClause: String) {
     NAME ("vf.displayName"),
     VIEW_COUNT ("vf.viewCount"),
     LAST_VIEWED ("vf.lastViewed"),
-    LAST_MODIFIED ("vf.lastModified")
+    LAST_MODIFIED ("vf.lastModified"),
+    FILE_ADDED ("vf.fileAdded")
 }
