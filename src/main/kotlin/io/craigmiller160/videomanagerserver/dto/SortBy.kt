9ae7d876b5b8c9014d6258e36616960c3701ddf1
Craig Miller <craigmiller160@gmail.com>
2019-04-24 21:54:26 -0400

    Fixed bugs in Sort By

--- src/main/kotlin/io/craigmiller160/videomanagerserver/dto/SortBy.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/dto/SortBy.kt
@@ -1,8 +1,8 @@
 package io.craigmiller160.videomanagerserver.dto
 
 enum class SortBy(val orderByClause: String) {
-    NAME ("vf.displayName, vf.fileName"),
-    VIEW_COUNT ("vf.view_count"),
-    LAST_VIEWED ("vf.last_viewed"),
-    LAST_MODIFIED ("vf.last_modified")
+    NAME ("vf.displayName"),
+    VIEW_COUNT ("vf.viewCount"),
+    LAST_VIEWED ("vf.lastViewed"),
+    LAST_MODIFIED ("vf.lastModified")
 }
