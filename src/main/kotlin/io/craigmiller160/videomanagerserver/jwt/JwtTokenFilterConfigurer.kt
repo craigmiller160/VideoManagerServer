e6caf58a03ebdebed7d422a1ca0a1ffcea463ad0
Craig Miller <craigmiller160@gmail.com>
2019-07-18 15:17:36 -0400

    Added configurer for token filter

---
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/jwt/JwtTokenFilterConfigurer.kt
@@ -0,0 +1,18 @@
+package io.craigmiller160.videomanagerserver.jwt
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class JwtTokenFilterConfigurer (
+        private val jwtTokenProvider: JwtTokenProvider
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(http: HttpSecurity?) {
+        http?.let {
+            val jwtFilter = JwtTokenFilter(jwtTokenProvider)
+            http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+        }
+    }
+}
