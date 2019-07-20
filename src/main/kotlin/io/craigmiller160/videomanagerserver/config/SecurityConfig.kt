df75657eca6f3b83ae509ac2e9e69c5e892413b8
Craig Miller <craigmiller160@gmail.com>
2019-07-20 11:25:59 -0400

    Got the error response code on authentication to be 401 instead of 403

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
@@ -4,11 +4,13 @@ import io.craigmiller160.videomanagerserver.jwt.JwtTokenFilterConfigurer
 import io.craigmiller160.videomanagerserver.jwt.JwtTokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
 @Configuration
 class SecurityConfig (
@@ -24,6 +26,9 @@ class SecurityConfig (
                     .and()
                     .sessionManagement()
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .exceptionHandling()
+                        .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 
             http.apply(JwtTokenFilterConfigurer(jwtTokenProvider))
         }
