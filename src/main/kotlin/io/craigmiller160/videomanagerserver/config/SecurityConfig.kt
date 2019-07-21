5c10e3b4c3c75f345a5a304aee115c078338997a
Craig Miller <craigmiller160@gmail.com>
2019-07-21 16:46:12 -0400

    Moved the hash rounds value to properties

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
@@ -6,24 +6,28 @@ import io.craigmiller160.videomanagerserver.security.AuthLoginFilter
 import io.craigmiller160.videomanagerserver.security.AuthSuccessHandler
 import io.craigmiller160.videomanagerserver.security.jwt.JwtTokenFilter
 import io.craigmiller160.videomanagerserver.security.service.AuthUserDetailsService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.validation.annotation.Validated
 
 @Configuration
+@Validated
 class SecurityConfig (
         private val authSuccessHandler: AuthSuccessHandler,
         private val authFailureHandler: AuthFailureHandler,
         private val authEntryPoint: AuthEntryPoint,
         private val jwtTokenFilter: JwtTokenFilter,
-        private val userDetailsService: AuthUserDetailsService
+        private val userDetailsService: AuthUserDetailsService,
+        @Value("\${video.security.password.hashRounds}")
+        private val hashRounds: Int
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
@@ -59,6 +63,6 @@ class SecurityConfig (
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder(12)
+        return BCryptPasswordEncoder(hashRounds)
     }
 }
