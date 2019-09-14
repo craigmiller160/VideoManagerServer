712cc8610e03e1be13c31274297585a5bca6bd11
Craig Miller <craigmiller160@gmail.com>
2019-09-14 03:50:39 -0400

    Optimized imports

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/WebMvcConfiguration.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/WebMvcConfiguration.kt
@@ -2,15 +2,10 @@ package io.craigmiller160.videomanagerserver.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
 class WebMvcConfiguration {
--- src/main/kotlin/io/craigmiller160/videomanagerserver/controller/AuthController.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/controller/AuthController.kt
@@ -2,7 +2,6 @@ package io.craigmiller160.videomanagerserver.controller
 
 import io.craigmiller160.videomanagerserver.dto.AppUser
 import io.craigmiller160.videomanagerserver.dto.Role
-import io.craigmiller160.videomanagerserver.dto.Token
 import io.craigmiller160.videomanagerserver.security.COOKIE_NAME
 import io.craigmiller160.videomanagerserver.security.ROLE_ADMIN
 import io.craigmiller160.videomanagerserver.service.security.AuthService
@@ -16,11 +15,9 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import kotlin.math.exp
 
 @RestController
 @RequestMapping("/auth")
