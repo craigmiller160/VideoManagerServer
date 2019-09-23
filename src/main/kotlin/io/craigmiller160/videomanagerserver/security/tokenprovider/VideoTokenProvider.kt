3e0687e182abbab047a14462e114d4e1269e8fbd
Craig Miller <craigmiller160@gmail.com>
2019-09-23 17:35:24 -0400

    Stubbed out VideoTokenProvider

--- src/main/kotlin/io/craigmiller160/videomanagerserver/security/tokenprovider/VideoTokenProvider.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/security/tokenprovider/VideoTokenProvider.kt
@@ -1,4 +1,39 @@
 package io.craigmiller160.videomanagerserver.security.tokenprovider
 
-class VideoTokenProvider {
+import io.craigmiller160.videomanagerserver.config.TokenConfig
+import io.craigmiller160.videomanagerserver.dto.AppUser
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+
+// TODO need unit tests for all methods here
+
+@Component
+class VideoTokenProvider (
+        private val tokenConfig: TokenConfig
+) : TokenProvider {
+
+    override fun createToken(user: AppUser): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun resolveToken(req: HttpServletRequest): String? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun validateToken(token: String): TokenValidationStatus {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createAuthentication(token: String): Authentication {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getClaims(token: String): Map<String, Any> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isRefreshAllowed(user: AppUser): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
