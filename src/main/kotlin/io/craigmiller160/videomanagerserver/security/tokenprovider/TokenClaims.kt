006ae293fdf9cff950c42325b67ecd3def6d2c22
Craig Miller <craigmiller160@gmail.com>
2019-09-23 17:33:41 -0400

    Fixed bad constant.

--- src/main/kotlin/io/craigmiller160/videomanagerserver/security/tokenprovider/TokenClaims.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/security/tokenprovider/TokenClaims.kt
@@ -2,7 +2,7 @@ package io.craigmiller160.videomanagerserver.security.tokenprovider
 
 object TokenClaims {
 
-    const val CLAIM_SUBJECT = "subject"
+    const val CLAIM_SUBJECT = "sub"
     const val CLAIM_ROLES = "roles"
 
 }
