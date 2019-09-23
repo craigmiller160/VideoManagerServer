825706a4694dc73a7cfe85eacebdbf5df589513c
Craig Miller <craigmiller160@gmail.com>
2019-09-23 17:38:27 -0400

    Stubbed out changes to AuthenticationFilter

--- src/main/kotlin/io/craigmiller160/videomanagerserver/security/AuthenticationFilter.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/security/AuthenticationFilter.kt
@@ -8,16 +8,30 @@ import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+// TODO need tests for the differing behavior for URLs
+
 class AuthenticationFilter (
         private val jwtTokenProvider: TokenProvider
 ) : OncePerRequestFilter() {
 
     public override fun doFilterInternal(req: HttpServletRequest, resp: HttpServletResponse, chain: FilterChain) {
+        TODO("Finish this with URL checks")
+    }
+
+    private fun validateVideoToken(req: HttpServletRequest, resp: HttpServletResponse, chain: FilterChain) {
+        TODO("Finish this")
+    }
+
+    private fun validateJwtToken(req: HttpServletRequest, resp: HttpServletResponse, chain: FilterChain) {
         val token = jwtTokenProvider.resolveToken(req)
         token?.let {
             try {
                 when (jwtTokenProvider.validateToken(token)) {
-                    TokenValidationStatus.VALID -> validToken(token, req, resp, chain)
+                    TokenValidationStatus.VALID -> {
+                        val auth = jwtTokenProvider.createAuthentication(token)
+                        SecurityContextHolder.getContext().authentication = auth
+                        chain.doFilter(req, resp)
+                    }
                     TokenValidationStatus.EXPIRED,
                     TokenValidationStatus.BAD_SIGNATURE,
                     TokenValidationStatus.NO_TOKEN -> unauthenticated(req, resp, chain)
@@ -28,12 +42,6 @@ class AuthenticationFilter (
                 unauthenticated(req, resp, chain)
             }
         } ?: unauthenticated(req, resp, chain)
-    }
-
-    private fun validToken(token: String, req: HttpServletRequest, resp: HttpServletResponse, chain: FilterChain) {
-        val auth = jwtTokenProvider.createAuthentication(token)
-        SecurityContextHolder.getContext().authentication = auth
-        chain.doFilter(req, resp)
     }
 
     private fun unauthenticated(req: HttpServletRequest, resp: HttpServletResponse, chain: FilterChain) {
