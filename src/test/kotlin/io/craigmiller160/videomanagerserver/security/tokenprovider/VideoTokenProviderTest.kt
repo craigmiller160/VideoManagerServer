16be6173679c09121d0e9c1bf6524491e60b84a8
Craig Miller <craigmiller160@gmail.com>
2019-09-29 07:45:40 -0400

    Began working on more test logic

--- src/test/kotlin/io/craigmiller160/videomanagerserver/security/tokenprovider/VideoTokenProviderTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/security/tokenprovider/VideoTokenProviderTest.kt
@@ -3,14 +3,24 @@ package io.craigmiller160.videomanagerserver.security.tokenprovider
 import io.craigmiller160.videomanagerserver.config.TokenConfig
 import io.craigmiller160.videomanagerserver.dto.AppUser
 import org.junit.Assert.assertFalse
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 @RunWith(MockitoJUnitRunner::class)
 class VideoTokenProviderTest {
+
+    companion object {
+        const val USER_NAME = "userName"
+        const val VIDEO_ID = "10"
+        private const val KEY = "ThisIsMySecretKeyThisIsMySecretKey"
+    }
 
     @Mock
     private lateinit var tokenConfig: TokenConfig
@@ -18,8 +28,23 @@ class VideoTokenProviderTest {
     @InjectMocks
     private lateinit var videoTokenProvider: VideoTokenProvider
 
+    private lateinit var secretKey: SecretKey
+
+    @Before
+    fun setup() {
+        val keyGen = KeyGenerator.getInstance("AES")
+        keyGen.init(256)
+        this.secretKey = keyGen.generateKey()
+        `when`(tokenConfig.secretKey)
+                .thenReturn(secretKey)
+    }
+
     @Test
     fun test_createToken() {
+        val appUser = AppUser(userName = USER_NAME)
+        val params = mapOf(TokenConstants.PARAM_VIDEO_ID to VIDEO_ID)
+        val token = videoTokenProvider.createToken(appUser, params)
+        println(videoTokenProvider.doDecrypt(token)) // TODO delete this
         TODO("Finish this")
     }
 
