5692c25d340873d51fdb792f6ca8a3426aafd219
Craig Miller <craigmiller160@gmail.com>
2020-01-03 15:43:22 -0500

    Implemented the first test for LocalFileController

--- src/test/kotlin/io/craigmiller160/videomanagerserver/controller/LocalFileControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/controller/LocalFileControllerTest.kt
@@ -1,12 +1,25 @@
 package io.craigmiller160.videomanagerserver.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.craigmiller160.videomanagerserver.dto.AppUser
+import io.craigmiller160.videomanagerserver.dto.LocalFile
+import io.craigmiller160.videomanagerserver.dto.Role
+import io.craigmiller160.videomanagerserver.security.ROLE_ADMIN
+import io.craigmiller160.videomanagerserver.security.tokenprovider.JwtTokenProvider
 import io.craigmiller160.videomanagerserver.service.file.LocalFileService
+import io.craigmiller160.videomanagerserver.test_util.responseBody
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasProperty
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.json.JacksonTester
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.ContextConfiguration
@@ -36,6 +49,14 @@ class LocalFileControllerTest {
     private lateinit var mockMvc: MockMvc
     private lateinit var mockMvcHandler: MockMvcHandler
 
+    @Autowired
+    private lateinit var jwtTokenProvider: JwtTokenProvider
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    private lateinit var jacksonLocalFileList: JacksonTester<List<LocalFile>>
+
     @Before
     fun setup() {
         mockMvc = MockMvcBuilders
@@ -44,11 +65,47 @@ class LocalFileControllerTest {
                 .alwaysDo<DefaultMockMvcBuilder>(MockMvcResultHandlers.print())
                 .build()
         mockMvcHandler = MockMvcHandler(mockMvc)
+
+        JacksonTester.initFields(this, objectMapper)
+    }
+
+    private fun mockFiles(): List<LocalFile> {
+        val file1 = LocalFile(
+                fileName = "file1",
+                filePath = "dir/file1",
+                isDirectory = false
+        )
+        val file2 = LocalFile(
+                fileName = "file2",
+                filePath = "dir/file2",
+                isDirectory = false
+        )
+        val dir1 = LocalFile(
+                fileName = "dir1",
+                filePath = "dir/dir1",
+                isDirectory = true
+        )
+        return listOf(file1, file2, dir1)
     }
 
     @Test
     fun test_getFilesFromDirectory() {
-        TODO("Finish this")
+        val user = AppUser(
+                userName = "userName",
+                roles = listOf(Role(name = ROLE_ADMIN))
+        )
+
+        val path = "dir"
+        val files = mockFiles()
+        `when`(localFileService.getFilesFromDirectory(path))
+                .thenReturn(files)
+
+        mockMvcHandler.token = jwtTokenProvider.createToken(user)
+        val response = mockMvcHandler.doGet("/api/localfiles/directory?path=$path")
+        assertThat(response, allOf(
+                hasProperty("status", equalTo(200)),
+                responseBody(equalTo(jacksonLocalFileList.write(files).json))
+        ))
     }
 
     @Test
