4f873e3662fa8d0439dad34ccf2281ddbd969eea
Craig Miller <craigmiller160@gmail.com>
2020-01-04 07:47:45 -0500

    Set up SettingsControllerTest

--- src/test/kotlin/io/craigmiller160/videomanagerserver/controller/SettingsControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/controller/SettingsControllerTest.kt
@@ -1,17 +1,63 @@
 package io.craigmiller160.videomanagerserver.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.craigmiller160.videomanagerserver.dto.Settings
+import io.craigmiller160.videomanagerserver.security.tokenprovider.JwtTokenProvider
+import io.craigmiller160.videomanagerserver.service.settings.SettingsService
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.json.JacksonTester
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @SpringBootTest
 @WebAppConfiguration
 @ContextConfiguration
 class SettingsControllerTest {
+
+    private lateinit var mockMvc: MockMvc
+    private lateinit var mockMvcHandler: MockMvcHandler
+
+    @MockBean
+    private lateinit var settingsService: SettingsService
+
+    @Autowired
+    private lateinit var settingsController: SettingsController
+
+    @Autowired
+    private lateinit var jwtTokenProvider: JwtTokenProvider
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var webAppContext: WebApplicationContext
+
+    private lateinit var jacksonSettings: JacksonTester<Settings>
+
+    @Before
+    fun setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webAppContext)
+                .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
+                .alwaysDo<DefaultMockMvcBuilder>(MockMvcResultHandlers.print())
+                .build()
+        mockMvcHandler = MockMvcHandler(mockMvc)
+
+        JacksonTester.initFields(this, objectMapper)
+    }
 
     @Test
     fun test_getSettings() {
