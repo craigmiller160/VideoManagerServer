4e032982c67ef7dd03a922d41aa99797d404b442
Craig Miller <craigmiller160@gmail.com>
2020-02-02 15:12:26 -0500

    Added missing @Repository annotations

--- src/main/kotlin/io/craigmiller160/videomanagerserver/repository/AppUserRepository.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/repository/AppUserRepository.kt
@@ -2,7 +2,9 @@ package io.craigmiller160.videomanagerserver.repository
 
 import io.craigmiller160.videomanagerserver.dto.AppUser
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AppUserRepository : CrudRepository<AppUser, Long> {
 
     fun findByUserName(userName: String): AppUser?
--- src/main/kotlin/io/craigmiller160/videomanagerserver/repository/SeriesRepository.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/repository/SeriesRepository.kt
@@ -2,5 +2,7 @@ package io.craigmiller160.videomanagerserver.repository
 
 import io.craigmiller160.videomanagerserver.dto.Series
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SeriesRepository : PagingAndSortingRepository<Series,Long>
