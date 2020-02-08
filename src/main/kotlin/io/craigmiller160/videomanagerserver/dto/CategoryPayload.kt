e89d7a42854ba0e17e791357d61d076cf0943c4e
Craig Miller <craigmiller160@gmail.com>
2020-02-08 13:44:58 -0500

    Created VideoFilePayload

--- src/main/kotlin/io/craigmiller160/videomanagerserver/dto/CategoryPayload.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/dto/CategoryPayload.kt
@@ -2,6 +2,5 @@ package io.craigmiller160.videomanagerserver.dto
 
 data class CategoryPayload (
         var categoryId: Long = 0,
-        var categoryName: String = "",
-        var hidden: Boolean = false
+        var categoryName: String = ""
 )
---
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/dto/VideoFilePayload.kt
@@ -0,0 +1,18 @@
+package io.craigmiller160.videomanagerserver.dto
+
+import io.craigmiller160.videomanagerserver.util.DEFAULT_TIMESTAMP
+import java.time.LocalDateTime
+
+data class VideoFilePayload (
+        var fileId: Long = 0,
+        var fileName: String = "",
+        var displayName: String = "",
+        var description: String = "",
+        var lastModified: LocalDateTime = DEFAULT_TIMESTAMP,
+        var fileAdded: LocalDateTime? = null,
+        var lastViewed: LocalDateTime? = null,
+        var viewCount: Int = 0,
+        var categories: MutableSet<CategoryPayload> = HashSet(),
+        var series: MutableSet<SeriesPayload> = HashSet(),
+        var stars: MutableSet<StarPayload> = HashSet()
+)
