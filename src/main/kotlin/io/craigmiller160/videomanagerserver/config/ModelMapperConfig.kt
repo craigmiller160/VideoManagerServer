b5791f05a2d058684d0f17c861065ebdcc351be1
Craig Miller <craigmiller160@gmail.com>
2020-02-08 13:52:36 -0500

    Stubbed out ModelMapperConfig and unit tests

---
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/ModelMapperConfig.kt
@@ -0,0 +1,15 @@
+package io.craigmiller160.videomanagerserver.config
+
+import org.modelmapper.ModelMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ModelMapperConfig {
+
+    @Bean
+    fun modelMapper(): ModelMapper {
+        TODO("Finish this")
+    }
+
+}
---
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/config/ModelMapperConfigTest.kt
@@ -0,0 +1,20 @@
+package io.craigmiller160.videomanagerserver.config
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class ModelMapperConfigTest {
+
+    @Test
+    fun test_convertVideoFileToVideoFilePayload() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_convertVideoFilePayloadToVideoFile() {
+        TODO("Finish this")
+    }
+
+}
