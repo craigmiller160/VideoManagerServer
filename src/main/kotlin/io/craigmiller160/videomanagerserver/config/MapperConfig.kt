fa22676edbc0028ea6f1387f0e0d8a99d71d17e3
Craig Miller <craigmiller160@gmail.com>
2020-02-15 15:30:50 -0500

    Added another TODO

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/MapperConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/MapperConfig.kt
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration
 class MapperConfig {
 
     @Bean
-    fun modelMapper(): VMModelMapper {
+    fun modelMapper(): VMModelMapper { // TODO this needs to be used in all places
         val videoFilePayloadHandler = VideoFilePayloadToVideoFileHandler()
         val categoryPayloadHandler = CategoryPayloadToCategoryHandler()
         val mapper = VMModelMapper()
