4fd5268911d105ef89631b39a482e74bf4291d11
Craig Miller <craigmiller160@gmail.com>
2020-02-15 12:44:03 -0500

    Got the main logic completed

--- src/main/kotlin/io/craigmiller160/videomanagerserver/mapper/ExistingPropHandlerKey.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/mapper/ExistingPropHandlerKey.kt
@@ -1,6 +1,6 @@
 package io.craigmiller160.videomanagerserver.mapper
 
 data class ExistingPropHandlerKey<S,D>(
-        val srcType: Class<S>,
-        val destType: Class<D>
+        val srcType: Class<out S>,
+        val destType: Class<out D>
 )
--- src/main/kotlin/io/craigmiller160/videomanagerserver/mapper/VMModelMapper.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/mapper/VMModelMapper.kt
@@ -5,16 +5,27 @@ import org.modelmapper.ModelMapper
 class VMModelMapper {
 
     private val mapper = ModelMapper()
-    val existingPropHandlers: Map<ExistingPropHandlerKey<*,*>,ExistingPropHandler<*,*>> = mutableMapOf()
+    val existingPropHandlers: MutableMap<ExistingPropHandlerKey<out Any,out Any>,ExistingPropHandler<out Any,out Any>> = mutableMapOf()
+
+    // TODO externalize this
+    init {
+        val handler = VideoFilePayloadToVideoFileHandler()
+        existingPropHandlers += handler.key to handler
+    }
 
     fun <D : Any> map(source: Any, destType: Class<D>): D {
         return mapper.map(source, destType)
     }
 
-    fun <D : Any> mapFromExisting(source: Any, existing: D): D {
+    fun <S: Any, D : Any> mapFromExisting(source: S, existing: D): D {
         val destType = existing::class.java
-        val result = map(source, destType)
-        TODO("Figure out how to properly map existing values onto result")
+        val destination = map(source, destType)
+        val key = ExistingPropHandlerKey<S,D>(source::class.java, destType) // TODO remove explicit type arguments once this works
+        val handler = existingPropHandlers[key] // TODO make sure this can handle null values
+        handler?.let {
+            (handler as ExistingPropHandler<S,D>).handleExisting(source, existing, destination)
+        }
+        return destination
     }
 
 }
