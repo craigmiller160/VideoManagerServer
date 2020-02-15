459679c0a55a07aade00f87c833d358b268a5e41
Craig Miller <craigmiller160@gmail.com>
2020-02-15 12:54:22 -0500

    Implemented unit tests for the handler classes

--- src/test/kotlin/io/craigmiller160/videomanagerserver/mapper/VideoFilePayloadToVideoFileHandlerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/mapper/VideoFilePayloadToVideoFileHandlerTest.kt
@@ -1,12 +1,34 @@
 package io.craigmiller160.videomanagerserver.mapper
 
+import io.craigmiller160.videomanagerserver.dto.VideoFilePayload
+import io.craigmiller160.videomanagerserver.entity.VideoFile
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasProperty
 import org.junit.Test
+import java.time.LocalDateTime
 
 class VideoFilePayloadToVideoFileHandlerTest {
 
+    private val handler = VideoFilePayloadToVideoFileHandler()
+
     @Test
     fun test_handleExisting() {
-        TODO("Finish this")
+        val source = VideoFilePayload()
+        val existing = VideoFile(
+                active = true,
+                lastScanTimestamp = LocalDateTime.now()
+        )
+        val destination = VideoFile(
+                fileName = "FileName"
+        )
+        handler.handleExisting(source, existing, destination)
+        assertThat(destination, allOf(
+                hasProperty("fileName", equalTo(destination.fileName)),
+                hasProperty("active", equalTo(existing.active)),
+                hasProperty("lastScanTimestamp", equalTo(existing.lastScanTimestamp))
+        ))
     }
 
 }
