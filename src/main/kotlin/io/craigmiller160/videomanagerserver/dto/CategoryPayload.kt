d3abdb5b909fe1d198766ab6bcbb627ad3e6a81c
Craig Miller <craigmiller160@gmail.com>
2020-02-15 12:01:37 -0500

    Added another TODO

--- src/main/kotlin/io/craigmiller160/videomanagerserver/dto/CategoryPayload.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/dto/CategoryPayload.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.videomanagerserver.dto
 
+// TODO hidden is not included here
 data class CategoryPayload (
         var categoryId: Long = 0,
         var categoryName: String = ""
