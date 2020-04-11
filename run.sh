b4094e4f416eeff9238ed9e434f9503421ba671f
Craig Miller <craigmiller160@gmail.com>
2020-04-11 09:53:47 -0400

    Got new configs and script working

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1"
+ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/"
 
 case $1 in
-  dev) mvn clean spring-boot:run -Dspring-boot.run.arguments="$ARGS" ;;
-  qa) mvn -P qa clean spring-boot:run -Dspring-boot.run.arguments="$ARGS" ;;
-  prod) mvn -P prod clean spring-boot:run -Dspring-boot.run.arguments="$ARGS" ;;
+  dev) mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
+  qa) mvn -P qa clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
+  prod) mvn -P prod clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
   *)
     echo "Invalid option: $1"
     exit 1
