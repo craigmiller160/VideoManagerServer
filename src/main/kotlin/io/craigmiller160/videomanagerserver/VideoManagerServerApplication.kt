42479d3bdcc0398f11f0b393660404695547e43a
Craig Miller <craigmiller160@gmail.com>
2020-04-11 15:08:54 -0400

    Got this working in prod

--- src/main/kotlin/io/craigmiller160/videomanagerserver/VideoManagerServerApplication.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/VideoManagerServerApplication.kt
@@ -4,7 +4,12 @@ import io.craigmiller160.videomanagerserver.security.AllowAllHostnameVerifier
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.ClassPathResource
+import java.security.KeyStore
 import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+
 
 @SpringBootApplication
 class VideoManagerServerApplication
@@ -12,9 +17,17 @@ class VideoManagerServerApplication
 private val logger = LoggerFactory.getLogger(VideoManagerServerApplication::class.java)
 
 fun main(args: Array<String>) {
-    val trustStorePath = ClassLoader.getSystemClassLoader().getResource("truststore.jks")?.toURI()?.path
-    System.setProperty("javax.net.ssl.trustStore", trustStorePath!!)
-    System.setProperty("javax.net.ssl.trustStorePassword", "changeit")
+    val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    val trustStore = KeyStore.getInstance("JKS")
+    val trustStoreStream = VideoManagerServerApplication::class.java.classLoader.getResourceAsStream("truststore.jks")
+    logger.info("" + trustStoreStream) // TODO delete this
+    trustStore.load(trustStoreStream, "changeit".toCharArray())
+    trustManagerFactory.init(trustStore)
+    val trustManagers = trustManagerFactory.trustManagers
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(null, trustManagers, null)
+    SSLContext.setDefault(sslContext)
+
     HttpsURLConnection.setDefaultHostnameVerifier(AllowAllHostnameVerifier())
 
     Thread.setDefaultUncaughtExceptionHandler { _, e -> logger.error("Uncaught exception", e) }
