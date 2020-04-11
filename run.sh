6e1b31204d71d625d751909a0c4cd98f161f7165
Craig Miller <craigmiller160@gmail.com>
2020-04-11 14:27:56 -0400

    Refactored start script and profiles and everything

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/"
+ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/ -Dspring.profiles.active=$1"
 
 case $1 in
-  dev) mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
-  qa) mvn -P qa clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
-  prod) mvn -P prod clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
+  dev|qa|prod) mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
   *)
     echo "Invalid option: $1"
     exit 1
