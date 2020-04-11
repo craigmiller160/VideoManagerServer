3ccd678e9ae97f5e3bce349c027fc845d7d667b0
Craig Miller <craigmiller160@gmail.com>
2020-04-11 17:17:57 -0400

    Feature/infra 15 (#29)
    * Some slight tweaks working on schema cleanup
    
    * Added cleanup of schema files
    
    * Upgraded spring parent pom and added actuator dependency
    
    * Added spring config client dependencies
    
    * Created new run.sh script
    
    * Refactored all the config files
    
    * Got new configs and script working
    
    * Added initial config for config server integration
    
    * Added notes to readme
    
    * Added https for config server
    
    * Trying to add a custom trust store to app
    
    * Trying to get the truststore to work
    
    * Got config server https working
    
    * Cleaned up some bootstrap.yml settings
    
    * Finally got everything working for config server
    
    * Began refactoring to use the shared configs
    
    * Moved more properties to shared configs
    
    * Moved TLS settings to shared config
    
    * Refactored start script and profiles and everything
    
    * Improvements to README.md
    
    * Updated prod bootstrap.yml uri
    
    * Got this working in prod
    
    * Cleaned up the code for setting up TLS
    
    * Got the cloud config retry working
    
    * Cleaned up the retry settings

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/ -Dspring.profiles.active=$1"
+
+case $1 in
+  dev|qa|prod) mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
+  *)
+    echo "Invalid option: $1"
+    exit 1
+  ;;
+esac
--- src/main/kotlin/io/craigmiller160/videomanagerserver/VideoManagerServerApplication.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/VideoManagerServerApplication.kt
@@ -1,16 +1,44 @@
 package io.craigmiller160.videomanagerserver
 
+import io.craigmiller160.videomanagerserver.security.AllowAllHostnameVerifier
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.ClassPathResource
+import java.security.KeyStore
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+
 
 @SpringBootApplication
 class VideoManagerServerApplication
 
+private const val TRUST_STORE_TYPE = "JKS"
+private const val TRUST_STORE_PATH = "truststore.jks"
+private const val TRUST_STORE_PASSWORD = "changeit"
+
 private val logger = LoggerFactory.getLogger(VideoManagerServerApplication::class.java)
 
 fun main(args: Array<String>) {
+    setupTls()
     Thread.setDefaultUncaughtExceptionHandler { _, e -> logger.error("Uncaught exception", e) }
     runApplication<VideoManagerServerApplication>(*args)
 }
 
+fun setupTls() {
+    val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+
+    val trustStore = KeyStore.getInstance(TRUST_STORE_TYPE)
+    val trustStoreStream = VideoManagerServerApplication::class.java.classLoader.getResourceAsStream(TRUST_STORE_PATH)
+    trustStore.load(trustStoreStream, TRUST_STORE_PASSWORD.toCharArray())
+
+    trustManagerFactory.init(trustStore)
+
+    val trustManagers = trustManagerFactory.trustManagers
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(null, trustManagers, null)
+    SSLContext.setDefault(sslContext)
+
+    HttpsURLConnection.setDefaultHostnameVerifier(AllowAllHostnameVerifier())
+}
