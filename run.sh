023af116ab2440f0fc47ec0ff53a21321e917e21
Craig Miller <craigmiller160@gmail.com>
2020-04-11 13:23:43 -0400

    Trying to add a custom trust store to app

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/"
+ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/ -Djavax.net.ssl.trustStore=truststore.jks -Djavax.net.ssl.trustStorePassword=SdLol?I#7p=Lz#i8BqE"
 
 case $1 in
   dev) mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
