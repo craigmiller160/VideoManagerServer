e5d6d36127bbb6c6c93c172d7a1d83025c87b775
Craig Miller <craigmiller160@gmail.com>
2020-06-16 21:21:45 -0400

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,37 @@ version=$(echo $raw_version | sed 's/^\s*<version>//g' | sed 's/<\/version>$//g'
 registry=localhost:32000
 tag=$registry/$name:$version
 
-echo "Building $name:$version"
+check_artifact_version() {
+  artifact_version=$(ls deploy/build | grep zip | sed 's/\.jar$//g' | sed "s/^$name-//g")
+  if [ $version != $artifact_version ]; then
+    echo "Project version $version does not equal artifact version $artifact_version"
+    exit 1
+  fi
+}
 
-cd deploy
-sudo docker build \
-  --network=host \
-  -t $tag \
-  .
-sudo docker push $tag
+check_deployment_version() {
+  deployment_version=$(cat deploy/deployment.yml | grep $registry | sed "s/^.*$registry\/$name://g")
+  if [ $version != $deployment_version ]; then
+    echo "Project version $version does not equal deployment.yml version $deployment_version"
+    exit 1
+  fi
+}
 
-sudo microk8s kubectl apply -f configmap.yml
-sudo microk8s kubectl apply -f deployment.yml
-sudo microk8s kubectl rollout restart deployment $name
+build() {
+  echo "Building $name:$version"
+
+  cd deploy
+  sudo docker build \
+    --network=host \
+    -t $tag \
+    .
+  sudo docker push $tag
+
+  sudo microk8s kubectl apply -f configmap.yml
+  sudo microk8s kubectl apply -f deployment.yml
+  sudo microk8s kubectl rollout restart deployment $name
+}
+
+check_artifact_version
+check_deployment_version
+build
