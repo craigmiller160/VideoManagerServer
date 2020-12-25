923576367526e1659ad1419218f1a850c75fe5fe
Craig Miller <craigmiller160@gmail.com>
2020-12-25 18:00:32 -0500

    Added request logger

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/WebConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/WebConfig.kt
@@ -19,15 +19,16 @@
 package io.craigmiller160.videomanagerserver.config
 
 import io.craigmiller160.webutils.controller.RequestLogger
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebUtilsConfig {
-    @Bean
-    @Order(0)
-    fun requestLogger(): RequestLogger {
-        return RequestLogger()
+class WebConfig : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(RequestLogger())
+            .addPathPatterns("/**")
     }
+
 }
