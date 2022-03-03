167bbf1645804155be1738aa14322e86ec7ce12b
Craig Miller <craigmiller160@gmail.com>
2022-03-03 08:23:42 -0500

    Code cleanup

--- src/main/kotlin/io/craigmiller160/videomanagerserver/controller/VideoUtils.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/controller/VideoUtils.kt
@@ -26,14 +26,9 @@ import kotlin.math.min
 fun resourceRegion(video: UrlResource, headers: HttpHeaders): ResourceRegion {
     val contentLength = video.contentLength()
     val range = headers.range.firstOrNull()
-    return if (range != null) {
-        val start = range.getRangeStart(contentLength)
-        val end = range.getRangeEnd(contentLength)
-        val rangeLength = min(1 * 1024 * 1024, end - start + 1)
-        ResourceRegion(video, start, rangeLength)
-    }
-    else {
-        val rangeLength = min(1 * 1024 * 1024, contentLength)
-        ResourceRegion(video, 0, rangeLength)
-    }
+    return range?.toResourceRegion(video)
+            ?: run {
+                val rangeLength = min(1 * 1024 * 1024, contentLength)
+                ResourceRegion(video, 0, rangeLength)
+            }
 }
