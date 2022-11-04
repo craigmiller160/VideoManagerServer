9ebf88a546840e8c47e6d2ca7d77f8ad06ce5a4e
Craig Miller <craigmiller160@gmail.com>
2022-11-04 09:25:07 -0400

    Some more fixes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ARGS="-Dspring.config.location=classpath:/config/common/,classpath:/config/$1/ -Dspring.profiles.active=$1"
 
 case $1 in
-  dev|qa|prod) mvnd clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
+  dev|qa|prod) mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="$ARGS" ;;
   *)
     echo "Invalid option: $1"
     exit 1
