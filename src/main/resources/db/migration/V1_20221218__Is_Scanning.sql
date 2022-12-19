b06e8ba7e3613dc3e69a081c66d7fd88a8d4192e
Craig Miller <craigmiller160@gmail.com>
2022-12-18 19:39:39 -0500

    Added schema

---
+++ src/main/resources/db/migration/V1_20221218__Is_Scanning.sql
@@ -0,0 +1,9 @@
+CREATE SEQUENCE is_scanning_id_seq START 1;
+CREATE TABLE is_scanning (
+    id BIGINT NOT NULL DEFAULT nextval('is_scanning_id_seq'::regclass),
+    is_scanning BOOLEAN NOT NULL DEFAULT FALSE,
+    version BIGINT NOT NULL DEFAULT 0
+    PRIMARY KEY (id)
+);
+
+INSERT INTO is_scanning (is_scanning) VALUES (false);
