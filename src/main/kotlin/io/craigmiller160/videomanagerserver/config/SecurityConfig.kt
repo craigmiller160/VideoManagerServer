4d12b86143f869053cc4a84fec506ce254e19f34
Craig Miller <craigmiller160@gmail.com>
2023-01-26 13:10:18 -0500

    More refactoring

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
@@ -18,89 +18,45 @@
 
 package io.craigmiller160.videomanagerserver.config
 
-import io.craigmiller160.spring.oauth2.security.JwtValidationFilterConfigurer
-import io.craigmiller160.videomanagerserver.security.AuthEntryPoint
 import io.craigmiller160.videomanagerserver.security.VideoAuthenticationFilterConfigurer
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @Validated
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 class SecurityConfig(
-  private val authEntryPoint: AuthEntryPoint,
-  private val videoAuthenticationFilterConfigurer: VideoAuthenticationFilterConfigurer,
-  @Value("\${video.security.password.hashRounds}") private val hashRounds: Int,
-  @Value("\${cors.origins}") private val corsOrigins: String,
-  private val jwtValidationFilterConfigurer: JwtValidationFilterConfigurer
-) : WebSecurityConfigurerAdapter() {
+  private val videoAuthenticationFilterConfigurer: VideoAuthenticationFilterConfigurer
+) : KeycloakWebSecurityConfigurerAdapter() {
 
-  override fun configure(http: HttpSecurity?) {
-    http?.let {
-      http
-        .csrf()
-        .disable()
-        .cors()
-        .configurationSource(corsConfigurationSource())
-        .and()
-        .authorizeRequests()
-        .antMatchers("/actuator/health")
-        .permitAll()
-        .antMatchers(*jwtValidationFilterConfigurer.getInsecurePathPatterns())
-        .permitAll()
-        .anyRequest()
-        .fullyAuthenticated()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-        .and()
-        .exceptionHandling()
-        .authenticationEntryPoint(authEntryPoint)
-        .and()
-        .apply(jwtValidationFilterConfigurer)
-        .and()
-        .apply(videoAuthenticationFilterConfigurer)
-        .and()
-        .requiresChannel()
-        .anyRequest()
-        .requiresSecure()
-    }
+  override fun configure(http: HttpSecurity) {
+    http
+      .csrf()
+      .disable()
+      .authorizeRequests()
+      .antMatchers("/actuator/health")
+      .permitAll()
+      .anyRequest()
+      .hasRole("access")
+      .and()
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+      .and()
+      .apply(videoAuthenticationFilterConfigurer)
+      .and()
+      .requiresChannel()
+      .anyRequest()
+      .requiresSecure()
   }
 
-  @Bean
-  fun passwordEncoder(): BCryptPasswordEncoder {
-    return BCryptPasswordEncoder(hashRounds)
-  }
-
-  @Bean
-  fun corsConfigurationSource(): CorsConfigurationSource {
-    val config = CorsConfiguration()
-    config.allowedOrigins = corsOrigins.split(",").map { it.trim() }.toList()
-    config.allowedMethods =
-      listOf(
-        HttpMethod.GET.name,
-        HttpMethod.DELETE.name,
-        HttpMethod.PUT.name,
-        HttpMethod.POST.name,
-        HttpMethod.OPTIONS.name)
-    config.allowedHeaders = listOf(HttpHeaders.AUTHORIZATION)
-    config.allowCredentials = true
-    val source = UrlBasedCorsConfigurationSource()
-    source.registerCorsConfiguration("/**", config)
-    return source
-  }
+  override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy =
+    NullAuthenticatedSessionStrategy()
 }
