e288fff11d726062c03807ce736ed434cdbfd8ff
Craig Miller <craigmiller160@gmail.com>
2023-01-26 13:12:11 -0500

    More work on configuring keycloak

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/SecurityConfig.kt
@@ -21,10 +21,12 @@ package io.craigmiller160.videomanagerserver.config
 import io.craigmiller160.videomanagerserver.security.VideoAuthenticationFilterConfigurer
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 import org.springframework.validation.annotation.Validated
@@ -59,4 +61,10 @@ class SecurityConfig(
 
   override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy =
     NullAuthenticatedSessionStrategy()
+
+  override fun configure(auth: AuthenticationManagerBuilder) {
+    val provider = keycloakAuthenticationProvider()
+    provider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+    auth.authenticationProvider(provider)
+  }
 }
