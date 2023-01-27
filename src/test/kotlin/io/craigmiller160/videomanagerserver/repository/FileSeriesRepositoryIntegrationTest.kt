cd8de7a6d00102604d1097fe54571876be91edcf
Craig Miller <craigmiller160@gmail.com>
2023-01-27 11:08:39 -0500

    Got more tests to pass

--- src/test/kotlin/io/craigmiller160/videomanagerserver/repository/FileSeriesRepositoryIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/repository/FileSeriesRepositoryIntegrationTest.kt
@@ -20,6 +20,7 @@ package io.craigmiller160.videomanagerserver.repository
 
 import io.craigmiller160.videomanagerserver.entity.Series
 import io.craigmiller160.videomanagerserver.entity.VideoFile
+import io.craigmiller160.videomanagerserver.test_util.AuthenticationConfig
 import io.craigmiller160.videomanagerserver.test_util.DbTestUtils
 import kotlin.test.assertEquals
 import org.hamcrest.MatcherAssert
@@ -35,6 +36,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
@@ -52,6 +54,7 @@ class FileSeriesRepositoryIntegrationTest {
   @Autowired private lateinit var fileSeriesRepo: FileSeriesRepository
   @Autowired private lateinit var videoFileRepo: VideoFileRepository
   @Autowired private lateinit var dbTestUtils: DbTestUtils
+  @MockBean private lateinit var authConfig: AuthenticationConfig
 
   private var fileId = 0L
   private var seriesId = 0L
