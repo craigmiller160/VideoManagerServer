ac15ee7ba45ae4d5c5148509c2da638b2c77dccf
Craig Miller <craigmiller160@gmail.com>
2023-04-11 17:10:49 -0400

    Working on setting up oauth2 client

--- src/main/kotlin/io/craigmiller160/videomanagerserver/config/RestConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/config/RestConfig.kt
@@ -1,12 +1,27 @@
 package io.craigmiller160.videomanagerserver.config
 
-import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory
-import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 
 @Configuration
-class RestConfig(private val keycloakClientRequestFactory: KeycloakClientRequestFactory) {
+class RestConfig {
   @Bean
-  fun restTemplate(): KeycloakRestTemplate = KeycloakRestTemplate(keycloakClientRequestFactory)
+  fun oauth2ClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+    authorizedClientRepository: OAuth2AuthorizedClientRepository
+  ): OAuth2AuthorizedClientManager {
+    val authorizedClientProvider =
+      OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
+
+    val authorizedClientManager =
+      DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository)
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+
+    return authorizedClientManager
+  }
 }
