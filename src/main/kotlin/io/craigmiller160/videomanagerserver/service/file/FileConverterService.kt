103e0fe26556aa56a3338185c6ef4a855b54b97d
Craig Miller <craigmiller160@gmail.com>
2023-04-11 09:04:40 -0400

    More work on conversion logic

--- src/main/kotlin/io/craigmiller160/videomanagerserver/service/file/FileConverterService.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/service/file/FileConverterService.kt
@@ -7,23 +7,16 @@ import ws.schild.jave.encode.AudioAttributes
 import ws.schild.jave.encode.EncodingAttributes
 import ws.schild.jave.encode.VideoAttributes
 import ws.schild.jave.encode.enums.X264_PROFILE
-import ws.schild.jave.info.VideoSize
+import ws.schild.jave.info.MultimediaInfo
 
 class FileConverterService {
   fun convert(source: File, target: File) {
     // TODO need to be able to set all of this based on the source
-    val audio = AudioAttributes()
-    audio.setCodec("aac")
-    audio.setBitRate(64000)
-    audio.setChannels(2)
-    audio.setSamplingRate(44100)
 
-    val video = VideoAttributes()
-    video.setCodec("h264")
-    video.setX264Profile(X264_PROFILE.BASELINE)
-    video.setBitRate(160000)
-    video.setFrameRate(15)
-    video.setSize(VideoSize(400, 300))
+    val mediaSource = MultimediaObject(source)
+    val info = mediaSource.info
+
+    val (audio, video) = mediaSource.let { getAudioVideoAttributes(it.info) }
 
     val attrs = EncodingAttributes()
     attrs.setInputFormat("mkv")
@@ -32,6 +25,29 @@ class FileConverterService {
     attrs.setVideoAttributes(video)
 
     val encoder = Encoder()
-    encoder.encode(MultimediaObject(source), target, attrs)
+    encoder.encode(mediaSource, target, attrs)
+  }
+
+  private fun getAudioVideoAttributes(
+    info: MultimediaInfo
+  ): Pair<AudioAttributes, VideoAttributes> {
+    val audio =
+      AudioAttributes().apply {
+        setCodec("aac")
+        setBitRate(info.audio.bitRate)
+        setChannels(info.audio.channels)
+        setSamplingRate(info.audio.samplingRate)
+      }
+
+    val video =
+      VideoAttributes().apply {
+        setCodec("h264")
+        setX264Profile(X264_PROFILE.BASELINE)
+        setBitRate(info.video.bitRate)
+        setFrameRate(info.video.frameRate.toInt())
+        setSize(info.video.size)
+      }
+
+    return audio to video
   }
 }
