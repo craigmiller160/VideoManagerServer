445ff37d463adeb0bbbabfa6d5442f66c0a40fa1
Craig Miller <craigmiller160@gmail.com>
2023-04-11 09:22:20 -0400

    Setup initial test

--- src/main/kotlin/io/craigmiller160/videomanagerserver/service/file/FileConverterService.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerserver/service/file/FileConverterService.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.videomanagerserver.service.file
 
 import java.io.File
+import java.nio.file.Paths
 import ws.schild.jave.Encoder
 import ws.schild.jave.MultimediaObject
 import ws.schild.jave.encode.AudioAttributes
@@ -8,6 +9,16 @@ import ws.schild.jave.encode.EncodingAttributes
 import ws.schild.jave.encode.VideoAttributes
 import ws.schild.jave.encode.enums.X264_PROFILE
 import ws.schild.jave.info.MultimediaInfo
+
+// TODO delete this
+fun main() {
+  val service = FileConverterService()
+  val source =
+    Paths.get(System.getProperty("user.home"), "Downloaders", "Temp", "movie.mkv").toFile()
+  val target =
+    Paths.get(System.getProperty("user.home"), "Downloaders", "Temp", "movie.mp4").toFile()
+  service.convert(source, target)
+}
 
 class FileConverterService {
   fun convert(source: File, target: File) {
