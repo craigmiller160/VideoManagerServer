a7e6074a60696e13af0d7a98eb1e518fcc87280f
Craig Miller <craigmiller160@gmail.com>
2023-04-12 14:00:20 -0400

    Fixed tests

--- src/test/kotlin/io/craigmiller160/videomanagerserver/config/VideoConfigurationIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerserver/config/VideoConfigurationIntegrationTest.kt
@@ -23,8 +23,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
@@ -39,6 +42,9 @@ class VideoConfigurationIntegrationTest {
 
   @Autowired private lateinit var videoConfig: VideoConfiguration
   @MockBean private lateinit var authConfig: AuthenticationConfig
+  @MockBean private lateinit var clientRegRepo: ClientRegistrationRepository
+  @MockBean private lateinit var authClientRepo: OAuth2AuthorizedClientRepository
+  @MockBean private lateinit var oauth2Props: OAuth2ClientProperties
 
   @Test
   fun testApiPageSize() {
